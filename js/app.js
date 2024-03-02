54982e9d9795058d1aa0613c7681ea366f7c6dae
Kinolog76 <vladpasha12@gmail.com>
2024-03-02 15:35:16 +0200

    Добавлены темы игр, парсинг и вывод слов из .json

--- js/app.js
+++ js/app.js
@@ -1,40 +1,57 @@
-let wordsArr = ["красный", "зеленый", "синий", "белый", "черный", "желтый", "розовый", "оранжевый", "фиолетовый", "голубой", "коричневый", "серый", "золотой", "серебряный", "бежевый"];
-const wordContainer = document.getElementById("word");
-const restart = document.getElementById("restart");
-const body = document.body;
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
-let randomWordNum = getRandomInt(wordsArr.length);
-let randomWord = wordsArr[randomWordNum];
-for (const letter of randomWord) {
-  wordContainer.innerHTML += `<span><p class="false">${letter}</p></span>`;
-}
 
-let buttons = document.querySelectorAll(".button__wrapper button");
-let word = document.querySelectorAll(".game__word span p");
-let errors = 1;
+const wordContainer = document.getElementById("word");
+const restart = document.querySelectorAll(".btn-restart");
 
-buttons.forEach((buttonElement) => {
-  buttonElement.addEventListener("click", function () {
-    let letterCheck = buttonElement.value.toLowerCase();
-    if (randomWord.includes(letterCheck)) {
-      console.log(letterCheck);
-      word.forEach((wordElement) => {
-        if (letterCheck == wordElement.innerHTML) {
-          wordElement.classList.add("true");
-          wordElement.classList.remove("false");
-          buttonElement.classList.add("btn-true");
+fetch("./js/database.json")
+  .then((response) => response.json())
+  .then((data) => {
+    let randomWordNum = getRandomInt(data.length);
+    let randomWordArray = data[randomWordNum].gameWord[getRandomInt(data[randomWordNum].gameWord.length)]
+    let randomWordLenght = randomWordArray.length
+    let buttons = document.querySelectorAll(".button__wrapper button");
+    const body = document.body;
+    let errors = 1;
+    let trueLetters = 0;
+
+    let gameTheme = document.querySelector('.theme__name')
+    gameTheme.innerHTML = data[randomWordNum].gameTheme;
+
+    for (const letter of randomWordArray) {
+      wordContainer.innerHTML += `<span><p class="false">${letter}</p></span>`;
+    }
+
+    let word = document.querySelectorAll(".game__word span p");
+
+    buttons.forEach((buttonElement) => {
+      buttonElement.addEventListener("click", function () {
+        let letterCheck = buttonElement.value.toLowerCase();
+        if (randomWordArray.includes(letterCheck)) {
+          word.forEach((wordElement) => {
+            if (letterCheck == wordElement.innerHTML) {
+              wordElement.classList.add("true");
+              wordElement.classList.remove("false");
+              buttonElement.classList.add("btn-true");
+              trueLetters++
+              if (trueLetters == randomWordLenght) {
+                body.classList.add("winner");
+              }
+            }
+          });
+        } else {
+          body.classList.add(`er-${errors++}`);
+          buttonElement.classList.add("btn-false");
         }
       });
-    } else {
-      body.classList.add(`er-${errors++}`);
-      buttonElement.classList.add("btn-false");
-    }
+    });
+  })
+  .catch((error) => console.error("Ошибка при загрузке JSON:", error));
+
+restart.forEach((btnRestart) => {
+  btnRestart.addEventListener("click", function () {
+    location.reload();
   });
 });
-
-restart.addEventListener("click", function(){
-  location.reload();
-})
